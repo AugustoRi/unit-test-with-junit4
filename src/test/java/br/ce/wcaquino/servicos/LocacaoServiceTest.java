5d00a7f4f9af7dce3d4d3ea151919ccfc2e25197
Augusto Ribeiro <franciscoaugustodev@gmail.com>
2024-04-02 16:25:11 -0300

    refactor: remove all unnecessary notations

--- src/test/java/br/ce/wcaquino/servicos/LocacaoServiceTest.java
+++ src/test/java/br/ce/wcaquino/servicos/LocacaoServiceTest.java
@@ -27,23 +27,7 @@ public class LocacaoServiceTest {
 
     @Before
     public void setup() {
-        System.out.println("Before");
         locacaoService = new LocacaoService();
-    }
-
-    @After
-    public void tearDown() {
-        System.out.println("After");
-    }
-
-    @BeforeClass
-    public static void setupClass() {
-        System.out.println("Before Class");
-    }
-
-    @AfterClass
-    public static void tearDownClass() {
-        System.out.println("After Class");
     }
 
     @Test
@@ -51,8 +35,6 @@ public class LocacaoServiceTest {
         //cenario
         Usuario usuario = new Usuario("Usuario 1");
         Filme filme = new Filme("Filme 1", 2, 5.0);
-
-        System.out.println("Teste!");
 
         //acao
         Locacao locacao = locacaoService.alugarFilme(usuario, filme);
